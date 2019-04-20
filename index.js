03ae3b56f2832d9d6448aa0cfcf2756cbe95185e
CatsAreMLG <joshuadgon@gmail.com>
2019-04-20 13:19:09 -0500

    Add dotenv

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const server = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT_ENV || 5000
 
 server.use(express.json())
-module.exports = server
 
-server.get('/', async (req, res) => {
-  res.status(200).json({ api: 'up' })
-})
+server.get('/', (req, res) => res.status(200).json({ api: 'up' }))
+
+server.listen(PORT, _ => console.log('Listening on port ' + PORT))
+
+module.exports = server
