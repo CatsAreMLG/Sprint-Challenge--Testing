0cff3c923bc60e125778a535a5cc62fbe62a4395
CatsAreMLG <joshuadgon@gmail.com>
2019-04-20 13:21:28 -0500

    Add games

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ server.use(express.json())
 
 server.get('/', (req, res) => res.status(200).json({ api: 'up' }))
 
+const games = [
+  {
+    title: 'Super Smash Bros',
+    genre: 'Fighting Platformer',
+    releaseYear: 1999
+  },
+  {
+    title: 'Minecraft',
+    genre: 'Voxel Adventure',
+    releaseYear: 2011
+  },
+  {
+    title: 'Super Mario World',
+    genre: 'Platformer',
+    releaseYear: 1990
+  }
+]
+
 server.listen(PORT, _ => console.log('Listening on port ' + PORT))
 
 module.exports = server
